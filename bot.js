26c7eb2844435eec79cffbb5233d517831711caa
Glitch (even-polite-prune) <none>
2020-10-28 19:31:21 +0000

    👽🎲 Checkpoint ./komutlar/kayıt.js:23040536/1442 ./package.json:23040536/1036 ./bot.js:23040536/32

---
+++ bot.js
@@ -0,0 +1,130 @@
+const discord = require('discord.js');
+const fs = require('fs');
+const http = require('http');
+const db = require('quick.db');
+const express = require('express');
+const ayarlar = require('./ayarlar.json');
+const app = express();
+
+//UPTİME İÇİN
+
+app.get("/", (request, response) => {
+  console.log("Hergün Yeni Bir Pinge Selamkee!!");
+  response.sendStatus(200);
+});
+app.listen(8000);
+setInterval(() => {
+  http.get(`https://cw-deneme-v12.glitch.me/`); //ALTYAPI ADRESİNİ GİRİN.
+}, 120000)
+
+//UPTİME İÇİN
+
+//READY.JS
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', async () => {
+   client.appInfo = await client.fetchApplication();
+  setInterval( async () => {
+    client.appInfo = await client.fetchApplication();
+  }, 600);
+  
+ client.user.setActivity(`discord.gg/codework`, { type:"WATHING" })
+  
+  console.log("CodeWork Akıyor!!")
+});
+
+const log = message => {
+  console.log(` ${message}`);
+};
+require('./util/eventLoader.js')(client);
+
+//READY.JS SON
+
+//KOMUT ALGILAYICI
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+    if (err) console.error(err);
+    log(`${files.length} komut yüklenecek.`);
+    files.forEach(f => {
+        let props = require(`./komutlar/${f}`);
+        log(`Yüklenen komut: ${props.help.name}.`);
+        client.commands.set(props.help.name, props);
+        props.conf.aliases.forEach(alias => {
+            client.aliases.set(alias, props.help.name);
+        });
+    });
+});
+
+
+
+
+client.reload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+           reject(e);
+        }
+    });
+};
+
+client.load = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+
+
+
+client.unload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+//KOMUT ALGILAYICI SON
+
+client.elevation = message => {
+    if (!message.guild) {
+        return;
+    }
+    let permlvl = 0;
+    if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+    if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+    if (message.author.id === ayarlar.sahip) permlvl = 4;
+    return permlvl;
+};
+client.login(process.env.TOKEN)
---
+++ komutlar/kayıt.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const db = require('quick.db');
+
+exports.run = async (client, message, args) => {
+
+  const codework = await db.fetch(`kayıtk_${message.guild.id}`)
+  
+  if(codework == null) return message.channel.send('');
+  if (message.channel.id !== codework) return message.channel.send(`Sadece Kayıt Kanalından Kayıt Olabilirsiniz.`);
+  if (codework == true) return; 
+  if (codework == false) return message.channel.send(`Bu Sunucuda Kayıt Sistemi Aktif Edilmemiş.`);
+ 
+  let user = message.member
+  let guild = message.guild
+  let isim = args[0];
+  let yas = args[1];
+  if (!isim) return message.channel.send(`İsmini girmelisin.`)
+  if (!yas) return message.channel.send(`Yaşını girmelisin.`)
+
+  user.setNickname(`[${isim}] [${yas}]`)
+  setTimeout(function(){user.addRole(db.fetch(`kayıt_${message.guild.id}`))},3000)
+  setTimeout(function(){user.removeRole(db.fetch(`kayıtalınacak_${message.guild.id}`))},4000)
+  message.channel.send(`${message.author} Sunucuya Başarıyla Kayıt oldun !`)
+  message.guild.channels.get(db.fetch(`kayıtlog_${message.guild.id}`)).send(`<a:kayit:707928076926976061> ${message.author} Adlı kullanıcı Başarılı Şekilde Kayıt Oldu ! <a:oldu:707928398902853713> `);
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0,
+  kategori: "yetkili"
+}
+
+exports.help = {
+  name: 'kayıt',
+  description: "Sunucuya kayıtolmaya yarar",
+  usage: 'kayıt <isim> <yaş>'
+}
