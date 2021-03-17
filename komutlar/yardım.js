34002910d114b2a7c2992b2d9e8738eb46258be7
Glitch (v12-kayit-altyapi) <none>
2021-03-17 20:53:29 +0000

    🎂👑 Checkpoint ./komutlar/yardım.js:26775989/1414

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -12,19 +12,25 @@ const mhelp = new Discord.MessageEmbed()
     .setDescription(`
 
 
-╔════════════════════════════════════╗
+╔═════════════════════════════════════╗
+║**MODERASYON KOMUTLARI**
 ║**!ban** : Sunucudan bir üyeyi yasaklar.
 ║**!unban** : İstediğiniz kişinin banını kaldırır.
 ║**!kick** : Sunucudan bir üye kickler.
+║**!jail** : Kişiyi jaile yollar.
+║══════════════════════════════════════
+║**SOHBET KOMUTLARI**
 ║**!sil** : Belirli bir kanaldaki mesajları siler.
 ║**!yavaş-mod** : Sohbete yazma sınır (süre) ekler.
 ║**!sohbet-kapat** Sohbeti Kapatır ve yazı yazılamaz.
 ║**!sohbet-aç** Sohbeti Açar ve yazı yazılabilir.
 ║**!nuke** Kanalı kapatır,mesajları siler ve tekrardan açar.
+║══════════════════════════════════════
+║**SESLİ KOMUTLARI**
 ║**!git** Kişi onay verirse odasına gidersiniz.
 ║**!çek** Kişi onay verirse odanıza gelir.
-║
-╚════════════════════════════════════╝
+║**!mute** Kişi onay verirse odanıza gelir.
+╚═════════════════════════════════════╝
 `) 
        .addField(`» Lrows V12 MODERASYON`, `**[Destek Sunucusu](https://discord.gg/Qfj3FRM)**`)
         .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.avatarURL)
@@ -35,12 +41,12 @@ message.channel.send(mhelp)
 exports.conf = {
     enabled: true, 
     guildOnly: false, 
-    aliases: ["mod"], 
+    aliases: ["yardım"], 
     permLevel: 0 
   };
  
   exports.help = {
-    name: 'moderasyon', 
+    name: 'yardım', 
     description: '',
     usage: ''
   };
